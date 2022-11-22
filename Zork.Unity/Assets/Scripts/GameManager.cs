02c011f8aed29947205bd03641154c80b495e13e
CasasJc <jesuscasas@knights.ucf.edu>
2022-11-22 09:00:00 -0500

    Zork unity progress commit 2

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -17,17 +17,40 @@ public class GameManager : MonoBehaviour
 
 
     [SerializeField]
-    private UnityInputService Input;
+    private UnityInputService InputService;
 
     [SerializeField]
-    private UnityOutputService Output;
+    private UnityOutputService OutputService;
 
     private void Awake()
     {
         TextAsset gameJson = Resources.Load<TextAsset>("GameJson");
         _game = JsonConvert.DeserializeObject<Game>(gameJson.text);
-        _game.Run(Input, Output);
+        _game.Run(InputService, OutputService);
     }
+
+    private void Start()
+    {
+        InputService.SetFocus();
+    }
+
+    private void Update()
+   {
+       if (Input.GetKeyDown(KeyCode.Return))
+       {
+            InputService.ProcessInput();
+       }
+
+       if(_game.IsRunning == false)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+
+        }
+   }
 
     private Game _game;
 }
